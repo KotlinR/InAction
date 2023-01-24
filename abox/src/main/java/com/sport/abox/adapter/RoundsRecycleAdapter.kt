06eb27a724971398d216380c9711c9ec52cbddc3
VadimRazzhivin <109168112+VadimRazzhivin@users.noreply.github.com>
2023-01-24 10:49:02 +0300

    Create training flow (#1)

---
+++ abox/src/main/java/com/sport/abox/adapter/RoundsRecycleAdapter.kt
@@ -0,0 +1,72 @@
+package com.sport.abox.adapter
+
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.sport.abox.R
+
+class RoundsRecycleAdapter(
+    private val exercises: MutableList<String>,
+    private val onLongClick: (Int) -> Unit,
+    private val onClick: (Int) -> Unit,
+) : RecyclerView.Adapter<RoundsRecycleAdapter.RoundViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RoundViewHolder {
+        return RoundViewHolder(
+            view = LayoutInflater
+                .from(parent.context)
+                .inflate(R.layout.round_and_exercises, parent, false),
+            onLongClick = onLongClick,
+            onClick = onClick,
+        )
+    }
+
+    override fun onBindViewHolder(holder: RoundViewHolder, position: Int) {
+        holder.onBind(exercises[position],position)
+    }
+
+    override fun getItemCount() = exercises.size
+
+    override fun onViewRecycled(holder: RoundViewHolder) {
+        super.onViewRecycled(holder)
+        holder.onUnbind()
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    fun updateRounds(allExercises: List<String>) {
+        exercises.clear()
+        exercises.addAll(allExercises)
+        notifyDataSetChanged()
+    }
+
+    class RoundViewHolder(
+        private val view: View,
+        private val onLongClick: (Int) -> Unit,
+        private val onClick: (Int) -> Unit,
+    ) : RecyclerView.ViewHolder(view) {
+        @SuppressLint("SetTextI18n")
+        fun onBind(exercise: String, position: Int) {
+            val roundOfTraining = view.findViewById<TextView>(R.id.tvRoundOfTraining)
+            val exerciseOfRound = view.findViewById<TextView>(R.id.tvExerciseOfTraining)
+            roundOfTraining.text = "Round [ ${position + 1} ]"
+            exerciseOfRound.text = exercise
+
+            view.setOnLongClickListener {
+                onLongClick(position)
+                true
+            }
+
+            view.setOnClickListener {
+                onClick(position)
+            }
+        }
+
+        fun onUnbind() {
+            view.setOnLongClickListener(null)
+            view.setOnClickListener(null)
+        }
+    }
+}
