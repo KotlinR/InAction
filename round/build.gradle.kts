1c98af2e2dca1c355180cd2f98018bd23c3991ef
VadimRazzhivin <vadimir14@gmail.com>
2023-07-02 16:30:09 +0300

    Update libs

--- round/build.gradle.kts
+++ round/build.gradle.kts
@@ -48,18 +48,18 @@ android {
 
 dependencies {
     implementation("androidx.lifecycle:lifecycle-livedata-core-ktx:2.6.1")
-    implementation("androidx.core:core-ktx:1.10.0")
+    implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.activity:activity-ktx:1.7.1")
-    implementation("com.google.android.material:material:1.8.0")
+    implementation("androidx.activity:activity-ktx:1.7.2")
+    implementation("com.google.android.material:material:1.9.0")
 
     // Room
-    implementation("androidx.room:room-runtime:2.5.1")
-    kapt("androidx.room:room-compiler:2.5.1")
+    implementation("androidx.room:room-runtime:2.5.2")
+    kapt("androidx.room:room-compiler:2.5.2")
 
     // Bottom Sheet
-    implementation("com.google.android.material:material:1.7.0-alpha03")
+    implementation("com.google.android.material:material:1.9.0")
 
     // Tests
     testImplementation("androidx.test.ext:junit-ktx:1.1.5")
@@ -75,8 +75,8 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
     // ActivityResultAPI
-    implementation("androidx.activity:activity-ktx:1.3.0-alpha02")
-    implementation("androidx.fragment:fragment-ktx:1.3.0")
+    implementation("androidx.activity:activity-ktx:1.7.2")
+    implementation("androidx.fragment:fragment-ktx:1.6.0")
 
     // FirebaseAnalytics
     implementation(platform("com.google.firebase:firebase-bom:32.1.1"))
